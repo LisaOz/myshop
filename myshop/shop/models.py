d686939797bb932022d3bcaa3c1f340e8206cb44
Lisa <frolovaie9@gmail.com>
2024-09-15 20:36:51 +0100

    Build Category and Product models with Meta classes for creating indexes.Establish one-to-many relationship between Category and Product models. Create initial migrations for the shop app and synchronize the database with migrations."

--- myshop/shop/models.py
+++ myshop/shop/models.py
@@ -1,3 +1,57 @@
 from django.db import models
 
 # Create your models here.
+
+"""
+Category model. This model consists of a name field and a unique slug field
+"""
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+
+
+    """
+    Define the index for the name field
+    """
+    class Meta:
+        ordering = ['name']
+        indexes = [models.Index(fields=['name']),]
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+    
+    def __str__(self):
+        return self.name
+
+
+"""
+Product model with fields category (Foreign Key to the Category model, one-to-many relationship; name, slug, image, description, price, availability, created, updated).
+"""
+class Product(models.Model):
+    category = models.ForeignKey(
+        Category, 
+        related_name='products', 
+        on_delete=models.CASCADE)
+    
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200)
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2) # Use DecimalField instead of FloatField to avoid rounding.
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+    """
+    Define a multiple field index for the id and slug fields indexed together as we plan to query products by both id and slug; one to many relationship.
+    """
+    class Meta:
+        ordering = ['name']
+        indexes = [
+            models.Index(fields=['id', 'slug']),
+            models.Index(fields=['name']),
+            models.Index(fields=['-created']),
+        ]
+    
+    def __str__(self):
+        return self.name
