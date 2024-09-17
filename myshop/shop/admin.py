52f801fd875274d8611aed2e7fd058d970b44722
Lisa <frolovaie9@gmail.com>
2024-09-17 12:02:08 +0100

    Implement administration site, create superuser, register catalog models on the admin site.

--- myshop/shop/admin.py
+++ myshop/shop/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Category, Product
 
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)} # value is automatically set from other fields
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = [
+        'name',
+        'slug',
+        'price',
+        'available',
+        'created',
+        'updated'
+    ]
+    list_filter = ['available', 'created', 'updated']
+    list_editable = ['price', 'available'] # fields can be edited from the display page of the admin site
+    prepopulated_fields = {'slug': ('name',)}
 # Register your models here.
